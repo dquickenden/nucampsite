c923d6c54b06ed5426ec82f8b92fdbe6957783a5
David Quickenden <david@quickenden.com>
2021-02-08 16:59:28 -0500

    Added SPA and React Router Parameters

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Footer(props) {
   return (
@@ -8,17 +9,15 @@ function Footer(props) {
           <div className="col-4 col-sm-2 offset-1">
             <h5>Links</h5>
             <ul className="list-unstyled">
-              <li>
-                <a href="#">Home</a>
+              <li><Link to="/home">Home</Link>
+              </li>
+              <li><Link to="/directory">Directory</Link>
               </li>
               <li>
-                <a href="#">Directory</a>
+                <Link to="/aboutus">About</Link>
               </li>
               <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
+                <Link to="/contactus">Contact</Link>
               </li>
             </ul>
           </div>
